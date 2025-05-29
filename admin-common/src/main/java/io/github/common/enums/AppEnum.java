e6c1bf17287e524bf0f3fc68a0e46ad93c5d8a35
chalmery <95135709+chalmery@users.noreply.github.com>
2025-05-29 23:56:48 +0800

    add：初始化多模块项目

---
+++ admin-common/src/main/java/io/github/common/enums/AppEnum.java
@@ -0,0 +1,30 @@
+package top.yangcc.common.enums;
+
+
+@SuppressWarnings("unused")
+public enum AppEnum {
+    AND("&"),
+
+    DOUBLE_AND("&&"),
+
+    OR("|"),
+
+    DOUBLE_OR("||"),
+
+    SLASH("/"),
+
+
+    LEFT_BRACKET("("),
+
+    RIGHT_BRACKET(")"),
+
+
+    ;
+
+    private final String code;
+
+    AppEnum(String code) {
+        this.code = code;
+    }
+
+}
--- admin-start/src/test/java/io/github/start/StartApplicationTests.java
+++ admin-start/src/test/java/io/github/start/StartApplicationTests.java
@@ -1,13 +1,13 @@
-package io.github.admin;
+package io.github.start;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AdminApplicationTests {
+class StartApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
